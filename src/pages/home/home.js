b92fa1ab1bbe95c2a0c6581038e36fa80b3745ba
Shubham Shekhar <shubham.ae.iitkgp@gmail.com>
2024-01-12 09:30:11 +0530

    styling

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -7,6 +7,9 @@ export default function Home() {
       <section className={styles.project_container}>
         <a href="/job-board">job-board</a>
         <a href="/like-button">like-button</a>
+        <a href="/paginator" className={styles.yet_implement}>
+          paginator
+        </a>
       </section>
 
       <h4>Css usage</h4>
@@ -15,6 +18,9 @@ export default function Home() {
         <a href="/grid-lights">grid-lights</a>
         <a href="/traffic-light">traffic-light</a>
         <a href="/star-rating">star-rating</a>
+        <a href="/page-theme" className={styles.yet_implement}>
+          page theme switch
+        </a>
       </section>
 
       <h4>Intermediate Apps</h4>
@@ -22,6 +28,8 @@ export default function Home() {
         <a href="/to-do-list">to-do-list</a>
         <a href="/temperature-converter">temperature-converter</a>
         <a href="/mortgage-calculator">mortgage-calculator</a>
+        <a className={styles.yet_implement}>Telephone-formatter</a>
+        <a className={styles.yet_implement}>Password-Strength</a>
       </section>
 
       <h4>Advanced Logic apps</h4>
@@ -30,6 +38,15 @@ export default function Home() {
         <a href="/transfer-list">transfer-list</a>
         <a href="/undoable-counter">undoable-counter</a>
         <a href="/generate-table">generate-table</a>
+        <a className={styles.yet_implement}>Comment-Box</a>
+        <a className={styles.yet_implement}>Basic-Calculator</a>
+        <a href="/typehead" className={styles.yet_implement}>
+          Google like typehead
+        </a>
+        <a className={styles.yet_implement}>Calendar</a>
+        <a href="/excel" className={styles.yet_implement}>
+          Microsoft like excel spreadsheet
+        </a>
       </section>
 
       <h4>Time apps</h4>
@@ -37,6 +54,7 @@ export default function Home() {
         <a href="/digital-clock">digital-clock</a>
         <a href="/stop-watch">stop-watch</a>
         <a href="/analog-clock">analog-clock</a>
+        <a className={styles.yet_implement}>Pomodoro</a>
       </section>
 
       <h4>Basic UI elements</h4>
@@ -48,6 +66,10 @@ export default function Home() {
         <a href="/progress-bars-3">progress-bars-3</a>
         <a href="/tabs">tabs</a>
         <a href="/skeleton-loader">Skeleton loader</a>
+        <a href="/spinning-loader">Spinning loader</a>
+        <a className={styles.yet_implement}>Dialog-popup</a>
+        <a className={styles.yet_implement}>Spinning-loader</a>
+        <a>slider with step</a>
       </section>
 
       <h4>Forms</h4>
@@ -55,6 +77,14 @@ export default function Home() {
         <a href="/sign-up-form">sign-up-form</a>
         <a href="/contact-form">contact-form</a>
         <a href="/flight-booker">flight-booker</a>
+        <a className={styles.yet_implement}>chips-input</a>
+        <a className={styles.yet_implement}>OTP input</a>
+        <a href="/file-uploader" className={styles.yet_implement}>
+          File uploader
+        </a>
+        <a href="/dynamic-forms" className={styles.yet_implement}>
+          Render dynamic forms using config
+        </a>
       </section>
 
       <h4>Games :</h4>
@@ -62,6 +92,8 @@ export default function Home() {
         <a href="/whack-mole-game">whack-mole-game</a>
         <a href="/memory-card-game">memory-card-game</a>
         <a href="/tic-tac-toe-game">tic-tac-toe-game</a>
+        <a className={styles.yet_implement}>Rock-paper-scissor</a>
+        <a className={styles.yet_implement}>Snake Game</a>
       </section>
 
       {/* coming soon */}
@@ -70,36 +102,12 @@ export default function Home() {
         <a href="https://sadanandpai.github.io/frontend-mini-challenges/javascript/">
           Sample External Frontend collection
         </a>
-        <a>File-uploader</a>
-        <a>Dialog-popup</a>
-        <a>Spinning-loader</a>
-        <a>Telephone-formatter</a>
-        <a>Rock-paper-scissor</a>
-        <a>Password-Strength</a>
         <a>Pixel-Art</a>
-        <a>JSon-creator</a>
-        <a>Comment-Box</a>
-        <a>Paginator</a>
-        <a>Calendar</a>
+        <a>JSON-creator</a>
         <a>Carousel</a>
-        <a>Pomodoro</a>
         <a>Sorting visusalizer</a>
-        <a>OTP input</a>
-        <a>BAsic-calucator</a>
-        <a>Infinite-scroll</a>
-        <a>chips-input</a>
-        <a>nav-bar</a>
-        <a>slider with step</a>
-
-        <a href="/typehead">Google like typehead</a>
-        <a href="/google-day-calendar">Google like day calendar</a>
-        <a href="/excel">Microsoft like excel spreadsheet</a>
-        <a href="/dynamic-forms">Render dynamic forms using config</a>
-        <a href="/cart-page">Design cart page</a>
-        <a href="/product-list-filters">Product listing page with filters</a>
-        <a href="/list-stopwatch-creation">
-          list of stopwatch creation and deletion
-        </a>
+        <a>Infinite-Scroll</a>
+        <a>Nav-Bar</a>
       </section>
     </main>
   );
